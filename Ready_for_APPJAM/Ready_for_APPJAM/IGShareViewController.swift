29debd2428085603fc48978c0a90aee5b84e4eba
정채은 <cce523@hanmail.net>
2023-06-26 02:03:45 +0900

    Feat [#3] instagram story share feature

--- Ready_for_APPJAM/Ready_for_APPJAM/IGShareViewController.swift
+++ Ready_for_APPJAM/Ready_for_APPJAM/IGShareViewController.swift
@@ -27,8 +27,9 @@ final class IGShareViewController: UIViewController {
     private lazy var shareButton = UIButton().then {
         $0.setTitle("인스타그램에 공유하기", for: .normal)
         $0.titleLabel?.font = .boldSystemFont(ofSize: 15)
+        $0.addTarget(self, action: #selector(shareButtonTapped), for: .touchUpInside)
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
@@ -37,9 +38,54 @@ final class IGShareViewController: UIViewController {
 }
 
 extension IGShareViewController {
+    
+    @objc private func shareButtonTapped() {
+        
+        if let storyShareURL = URL(string: "instagram-stories://share?source_application=1196046794406519") {
+            if UIApplication.shared.canOpenURL(storyShareURL)
+            {
+                
+                
+                let renderer = UIGraphicsImageRenderer(size: backgroundView.bounds.size)
+                
+                let renderImage = renderer.image { _ in
+                    backgroundView.drawHierarchy(in: backgroundView.bounds, afterScreenUpdates: true)
+                }
+                
+                
+                guard let imageData = renderImage.pngData() else {return}
+                
+                
+                
+                let pasteboardItems : [String:Any] = [
+                    "com.instagram.sharedSticker.stickerImage": imageData,
+                    "com.instagram.sharedSticker.backgroundTopColor" : "#000000",
+                    "com.instagram.sharedSticker.backgroundBottomColor" : "#000000",
+                    
+                ]
+                let pasteboardOptions = [
+                    UIPasteboard.OptionsKey.expirationDate : Date().addingTimeInterval(300)
+                ]
+                
+                UIPasteboard.general.setItems([pasteboardItems], options: pasteboardOptions)
+                
+                
+                UIApplication.shared.open(storyShareURL, options: [:], completionHandler: nil)
+            }
+            else
+            {
+                let alert = UIAlertController(title: "알림", message: "인스타그램이 필요합니다", preferredStyle: .alert)
+                let ok = UIAlertAction(title: "확인", style: .default, handler: nil)
+                alert.addAction(ok)
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    
+    
     private func setUI() {
         view.backgroundColor = .black
-        
     }
     
     private func setLayout() {
