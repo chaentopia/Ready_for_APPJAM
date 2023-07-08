a52bd7131eb33d40af8adde7d9f6ecea232c27b7
정채은 <cce523@hanmail.net>
2023-07-08 19:26:53 +0900

    Feat [#15] 프로필 뷰 레이아웃 설정

--- Ready_for_APPJAM/Ready_for_APPJAM/Profile/ProfileView.swift
+++ Ready_for_APPJAM/Ready_for_APPJAM/Profile/ProfileView.swift
@@ -6,15 +6,59 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-class ProfileView: UIView {
+final class ProfileView: UIView {
+    
+    private let profileLabel = UILabel()
+    private let myProfileView = MyProfileView()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
     }
-    */
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
+extension ProfileView {
+    private func setUI() {
+        setStyle()
+        setLayout()
+    }
+    
+    private func setStyle() {
+        self.backgroundColor = .black
+        profileLabel.do {
+            $0.text = "프로필"
+            $0.textColor = .white
+            $0.font = .boldSystemFont(ofSize: 20)
+        }
+    }
+    
+    private func setLayout() {
+        self.addSubview(profileLabel)
+        self.addSubview(myProfileView)
+        
+        profileLabel.snp.makeConstraints {
+            let statusBarHeight = UIApplication.shared.connectedScenes
+                        .compactMap { $0 as? UIWindowScene }
+                        .first?
+                        .statusBarManager?
+                        .statusBarFrame.height ?? 20
+            
+            $0.top.equalTo(self.safeAreaInsets).offset(statusBarHeight + 22.adjusted)
+            $0.leading.equalToSuperview().offset(16.adjusted)
+        }
+        
+        myProfileView.snp.makeConstraints {
+            $0.top.equalTo(profileLabel.snp.bottom).offset(22)
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(230)
+        }
+    }
 }
