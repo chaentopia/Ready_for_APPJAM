e2bf1c87fe281c4524635b29a9655e3aa170592a
정채은 <cce523@hanmail.net>
2023-07-08 19:25:06 +0900

    Chore [#15] SceneDelegate 수정

--- Ready_for_APPJAM/Ready_for_APPJAM/SceneDelegate.swift
+++ Ready_for_APPJAM/Ready_for_APPJAM/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // 2.
         self.window = UIWindow(windowScene: windowScene)
     // 3.
-        let navigationController = UINavigationController(rootViewController: RecommendingViewController())
+        let navigationController = UINavigationController(rootViewController: ProfileViewController())
         self.window?.rootViewController = navigationController
     // 4.
         self.window?.makeKeyAndVisible()
