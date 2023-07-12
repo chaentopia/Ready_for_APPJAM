01e73eddbc0694f02773a74a6f2feb52069c7370
정채은 <cce523@hanmail.net>
2023-07-12 15:17:56 +0900

    Fix [#15] 이런저런 것들 ^^

--- Ready_for_APPJAM/Ready_for_APPJAM/SceneDelegate.swift
+++ Ready_for_APPJAM/Ready_for_APPJAM/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // 2.
         self.window = UIWindow(windowScene: windowScene)
     // 3.
-        let navigationController = UINavigationController(rootViewController: ProfileViewController())
+        let navigationController = UINavigationController(rootViewController: RecommendingViewController())
         self.window?.rootViewController = navigationController
     // 4.
         self.window?.makeKeyAndVisible()
